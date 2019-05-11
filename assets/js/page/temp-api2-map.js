701cadb0f7ae98fca0e6a8d307b0b86cb50be8b8
coregameHD <thewegang@gmail.com>
2019-05-11 21:02:05 +0700

    Minor improvements

--- assets/js/page/temp-api2-map.js
+++ assets/js/page/temp-api2-map.js
@@ -5,8 +5,8 @@ var array_maplongitude = [];
 
 var array_marker = [];
 
-$.getJSON("https://lapsscentral.azurewebsites.net/api/nodeinfos", function(data2) {
-    for (var i = 0; i < data2.length; i++){
+$.getJSON("https://lapsscentral.azurewebsites.net/api/nodeinfos", function(data) {
+    for (var i = 0; i < data.length; i++){
         array_mapid.push(data[i].id);
         array_mapname.push(data[i].name);
         array_maplatitude.push(data[i].latitude);
