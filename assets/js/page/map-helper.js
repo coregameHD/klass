5623c2cb0d10a9f8853f51dca7d39fcbb7353f24
coregameHD <thewegang@gmail.com>
2019-05-13 20:05:35 +0700

    Add 'Sensors Information History'

--- assets/js/page/map-helper.js
+++ assets/js/page/map-helper.js
@@ -2,46 +2,45 @@
 
 // Helper Function: Generate marker with a number
 function generateMarker(aqi) {
-  var canvas = document.createElement('canvas');
-  canvas.height = 64;
-  canvas.width = 64;
+    var canvas = document.createElement('canvas');
+    canvas.height = 64;
+    canvas.width = 64;
 
-  var ctx = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
 
-  if(aqi <= 50) {
-      ctx.fillStyle = "green";
-  } else if(aqi >= 51 && aqi <= 100) {
-      ctx.fillStyle = "yellow";
-  } else if(aqi >= 101 && aqi <= 150) {
-      ctx.fillStyle = "orange";
-  } else if(aqi >= 151){
-      ctx.fillStyle = "red";
-  }
+    if (aqi <= 50) {
+        ctx.fillStyle = "green";
+    } else if (aqi >= 51 && aqi <= 100) {
+        ctx.fillStyle = "yellow";
+    } else if (aqi >= 101 && aqi <= 150) {
+        ctx.fillStyle = "orange";
+    } else if (aqi >= 151) {
+        ctx.fillStyle = "red";
+    }
 
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "black";
-  ctx.beginPath();
-  ctx.arc(32, 32, 16, 0, Math.PI * 2, true);
-  ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.arc(32, 32, 16, 0, Math.PI * 2, true);
+    ctx.fill();
 
-  if(aqi >= 51 && aqi <= 150) {
-      ctx.fillStyle = "black";
-  } else {
-      ctx.fillStyle = "white";
-  }
-  ctx.textAlign = "center";
-  ctx.font = "12px Lucida Console";
-  ctx.fillText(aqi, 32, 35, 64);
-  ctx.stroke();
+    if (aqi >= 51 && aqi <= 150) {
+        ctx.fillStyle = "black";
+    } else {
+        ctx.fillStyle = "white";
+    }
+    ctx.textAlign = "center";
+    ctx.font = "12px Lucida Console";
+    ctx.fillText(aqi, 32, 35, 64);
+    ctx.stroke();
 
-  var marker = canvas.toDataURL("image/png;base64");
-  return marker;
+    var marker = canvas.toDataURL("image/png;base64");
+    return marker;
 }
 
 // Helper Function: Display pop-up when user clicked on the marker
-function displayPopup(marker, title, content){
-  google.maps.event.addListener(marker, "click", function (e) {
-      swal(title, content);
-    }
-  );
+function displayPopup(marker, title, content) {
+    google.maps.event.addListener(marker, "click", function (e) {
+        swal(title, content);
+    });
 }
--- assets/js/page/table.js
+++ assets/js/page/table.js
@@ -1,29 +1,44 @@
 // Sensors Information Table
-$.getJSON("https://lapsscentral.azurewebsites.net/api/sensors", function(data) {
+$.getJSON("https://lapsscentral.azurewebsites.net/api/sensors", function (data) {
   var table = '<div class="table-responsive">';
   table += '<table class="table table-striped table-md">'
-	table += "<tr><th>#</th><th>Name</th><th>PM 2.5</th><th>PM 10</th><th>PM 1</th><th>Temperature</th><th>Humidity</th><th>Record On</th></tr>"; 
-  for (var i = 0; i < data.length; i++){
-    table += "<tr><td>"+i+"</td><td>"+data[i].name+"</td><td>"+data[i].pm25Level+"</td><td>"+data[i].pm10Level+"</td><td>"+data[i].pm1Level+"</td><td>"+data[i].temp+"</td><td>"+data[i].humidity+"</td><td>"+data[i].recordedOn+"</td></tr>";
+  table += "<tr><th>#</th><th>Name</th><th>PM 2.5</th><th>PM 10</th><th>PM 1</th><th>Temperature</th><th>Humidity</th><th>Record On</th></tr>";
+  for (var i = 0; i < data.length; i++) {
+    table += "<tr><td>" + i + "</td><td>" + data[i].name + "</td><td>" + data[i].pm25Level + "</td><td>" + data[i].pm10Level + "</td><td>" + data[i].pm1Level + "</td><td>" + data[i].temp + "</td><td>" + data[i].humidity + "</td><td>" + data[i].recordedOn + "</td></tr>";
   }
   table += "</table>";
   table += "</div>";
 
   //Showing the table inside html
-  document.getElementById("table_sensors").innerHTML = table; 
+  document.getElementById("table_sensors").innerHTML = table;
+});
+
+// Sensor Information History Table
+$.getJSON("https://lapsscentral.azurewebsites.net/api/sensors/history?limit=10", function (data) {
+  var table = '<div class="table-responsive">';
+  table += '<table class="table table-striped table-md">'
+  table += "<tr><th>#</th><th>Name</th><th>PM 2.5</th><th>PM 10</th><th>PM 1</th><th>Temperature</th><th>Humidity</th><th>Record On</th></tr>";
+  for (var i = 0; i < data.length; i++) {
+    table += "<tr><td>" + i + "</td><td>" + data[i].name + "</td><td>" + data[i].pm25Level + "</td><td>" + data[i].pm10Level + "</td><td>" + data[i].pm1Level + "</td><td>" + data[i].temp + "</td><td>" + data[i].humidity + "</td><td>" + data[i].recordedOn + "</td></tr>";
+  }
+  table += "</table>";
+  table += "</div>";
+
+  //Showing the table inside html
+  document.getElementById("table_sensors_history").innerHTML = table;
 });
 
 // Node Information Table
-$.getJSON("https://lapsscentral.azurewebsites.net/api/nodeinfos", function(data) {
+$.getJSON("https://lapsscentral.azurewebsites.net/api/nodeinfos", function (data) {
   var table = '<div class="table-responsive">';
   table += '<table class="table table-striped table-md">'
-	table += "<tr><th>#</th><th>Name</th><th>Latitude</th><th>Longitude</th><th>Status</th></tr>"; 
-  for (var i = 0; i < data.length; i++){
-    table += "<tr><td>"+i+"</td><td>"+data[i].name+"</td><td>"+data[i].latitude+"</td><td>"+data[i].longitude+"</td><td><div class='badge badge-success'>Active</div></td></tr>";
+  table += "<tr><th>#</th><th>Name</th><th>Latitude</th><th>Longitude</th><th>Status</th></tr>";
+  for (var i = 0; i < data.length; i++) {
+    table += "<tr><td>" + i + "</td><td>" + data[i].name + "</td><td>" + data[i].latitude + "</td><td>" + data[i].longitude + "</td><td>Active</td></tr>";
   }
   table += "</table>";
   table += "</div>";
 
   //Showing the table inside html
-  document.getElementById("table_nodeinfos").innerHTML = table; 
+  document.getElementById("table_nodeinfos").innerHTML = table;
 });
