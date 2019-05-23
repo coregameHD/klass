a34cdc90e98aa26c4eb32bf4807ca0413cb973ce
coregameHD <thewegang@gmail.com>
2019-05-23 21:35:11 +0700

    API docs updated

--- assets/js/page/table.js
+++ assets/js/page/table.js
@@ -17,7 +17,7 @@ $.getJSON("https://api.klass.greenkmitl.com/api/sensors", function (data) {
 $.getJSON("https://api.klass.greenkmitl.com/api/sensors/history?limit=10", function (data) {
   var table = '<div class="table-responsive">';
   table += '<table class="table table-bordered table-md table-hover">'
-  table += "<tr><th>#</th><th>Name</th><th>PM 2.5</th><th>PM 10</th><th>PM 1</th><th>Temperature</th><th>Humidity</th><th>Record On</th></tr>";
+  table += "<thead class='thead-light'><tr><th>#</th><th>Name</th><th>PM 2.5</th><th>PM 10</th><th>PM 1</th><th>Temperature</th><th>Humidity</th><th>Record On</th></tr></thead>";
   for (var i = 0; i < data.length; i++) {
     table += "<tr><td>" + (i+1) + "</td><td>" + data[i].name + "</td><td>" + data[i].pm25Level + "</td><td>" + data[i].pm10Level + "</td><td>" + data[i].pm1Level + "</td><td>" + data[i].temp + "</td><td>" + data[i].humidity + "</td><td>" + new Date(data[i].recordedOn).toLocaleString() + "</td></tr>";
   }
